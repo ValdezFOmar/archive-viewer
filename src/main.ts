1ab203d4b432bde173c5d28ba3fcaedbb227e351
Omar Valdez <omarantoniovaldezf2@gmail.com>
2026-09-06 02:08:17 -0700

    feat: add progress bar for archive download progress

--- src/main.ts
+++ src/main.ts
@@ -22,33 +22,82 @@ function formatFileSize(bytes: number): string {
     return megabytes.toFixed(0) + ' MB';
 }
 
+// NOTE: Always consume the response, otherwise it could cause performance/memory problems.
+async function updateProgressBar(progress: HTMLProgressElement, response: Response) {
+    if (response.body) {
+        const contentLength = response.headers.get('Content-Length');
+        const totalBytes = contentLength ? Number.parseInt(contentLength, 10) : null;
+        if (totalBytes) {
+            progress.max = totalBytes;
+        } else {
+            progress.removeAttribute('value'); // make progress indeterminate
+        }
+        let bytes = 0;
+        const reader = response.body.getReader();
+        while (true) {
+            const { value, done } = await reader.read();
+            if (done) {
+                break;
+            }
+            bytes += value.length;
+            // updating 'value' only makes sense when there's a know 'max'
+            if (totalBytes) {
+                progress.value = bytes;
+            }
+        }
+    }
+}
+
 document.getElementById('view-form')!.addEventListener('submit', async event => {
     event.preventDefault();
 
     const input = document.getElementById('url') as HTMLInputElement;
     const button = document.getElementById('view-button') as HTMLButtonElement;
+    const progress = document.querySelector('progress')!;
     const archiveView = document.getElementById('archive-view')!;
     const archiveDetails = document.getElementById('archive-details')!;
     const archiveEntries = document.getElementById('archive-entries')!;
 
     const archiveUrl = input.value;
-    const httpReader = new HttpReader(archiveUrl);
+    const httpReader = new HttpReader(archiveUrl, {
+        fetch: async (input, init) => {
+            const isGet = !init?.method || init.method === 'GET';
+            const response = await fetch(input, init);
+            if (isGet && response.ok) {
+                updateProgressBar(progress, response.clone());
+            }
+            return response;
+        },
+    });
     const zipReader = new ZipReader(httpReader);
 
     let entries: Entry[];
     try {
         input.disabled = true;
         button.disabled = true;
+        progress.max = 1;
+        progress.value = 0;
+        progress.hidden = false;
         entries = await zipReader.getEntries();
     } catch (error) {
         alert('Could not preview file');
-        console.debug(error);
+        console.error(error);
         return;
     } finally {
         input.disabled = false;
         button.disabled = false;
+        progress.hidden = true;
+        await zipReader.close();
     }
 
+    // TODO:
+    // To provide better sorting, transform the entries array and parse
+    // the 'filename' property into directory components and the actual filename,
+    // then entries can be sorted in order:
+    //  - depth (top level files first, nested files later)
+    //  - directory name (for directories at the same depth)
+    //  - file name (for files within the same directory)
+    //  - extension (when all other values are the same)
     entries.sort((entry1, entry2) => {
         const [name1, ext1] = splitFileName(entry1.filename);
         const [name2, ext2] = splitFileName(entry2.filename);
@@ -77,19 +126,19 @@ document.getElementById('view-form')!.addEventListener('submit', async event => 
         if (!entry.directory) {
             fileCount++;
             if (/\.(txt|py|md|json|csv|xml|html)$/.test(filePath)) {
-                const fragment = document.importNode(textTemplate.content, true);
-                const pre = fragment.querySelector('pre')!;
                 const text = await entry.getData(new TextWriter());
                 if (text !== '') {
-                    pre.textContent = await entry.getData(new TextWriter());
-                    pathContainer.parentElement?.append(fragment);
+                    const node = document.importNode(textTemplate.content, true);
+                    const pre = node.querySelector('pre')!;
+                    pre.textContent = text;
+                    pathContainer.parentElement?.append(node);
                 }
             } else if (/\.(png|jpg|jpeg|gif)$/.test(filePath)) {
-                const fragment = document.importNode(imageTemplate.content, true);
-                const img = fragment.querySelector('img')!;
+                const node = document.importNode(imageTemplate.content, true);
+                const img = node.querySelector('img')!;
                 const blob = await entry.getData(new BlobWriter());
                 img.src = URL.createObjectURL(blob);
-                container.append(fragment);
+                container.append(node);
             }
         }
 
